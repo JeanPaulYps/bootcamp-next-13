619c3044cc53c4d70bd818f144d5ff677db2e381
Jean Paul <yjeanpaul@gmail.com>
2024-01-10 20:10:18 -0500

    Added dynamic restaurants for the root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,47 @@
+import { Cuisine, Location, PRICE, PrismaClient } from "@prisma/client";
 import Head from "next/head";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import RestaurantCard from "./components/RestaurantCard";
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
+}
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants: RestaurantCardType[] = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+      slug: true,
+    },
+  });
+
+  return restaurants;
+};
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
   return (
     <>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestaurantCard />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
       </div>
     </>
   );
